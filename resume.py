344ff4623f8502e39f7c6c2600f91d355d8e3178
Shin Choo <60047262+shinchoo@users.noreply.github.com>
2026-04-24 22:53:19 -0400

    Update resume.py with new content and links

--- resume.py
+++ resume.py
@@ -27,7 +27,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Shin H. Choo</a>
+  <a class="navbar-brand" href="https://shinchoo.github.io/" target="_blank">Shin H. Choo</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
